f1d6a8e9107d26b9f457a691acbc701b14d65e58
Malte Bargholz <malte@screenri.de>
2017-07-26 01:19:40 +0200

    add strict mode to main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+'use strict;' //enable strict compiler mode
+//this disables some legacy features such as assignment without declaration
+//for more information see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Strict_mode
+
+//express init
 let express = require('express'); //import express framework
 let app = express();  //get the global express instance
 let port = process.env.PORT || 3000; //use supplied port or default 3000;
