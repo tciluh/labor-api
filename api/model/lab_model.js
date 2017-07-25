1539e5feea5027f413f8df0243ad6f41783717b1
Malte Bargholz <malte@screenri.de>
2017-07-26 01:55:06 +0200

    add stub model for SOPs

---
+++ api/model/lab_model.js
@@ -0,0 +1,16 @@
+'use strict;' //use strict mode. see main.js
+
+//mongoose import
+let Mongoose = require('mongoose');
+
+//define a schema for an SOP Object inside MongoDB
+let SOPSchema = new Mongoose.Schema({
+
+});
+
+//compile the model into a usable class
+let SOPObject = Mongoose.model('SOP', SOPSchema);
+
+module.exports = {
+    SOP: SOPObject //export the SOPObject as SOP to including classes
+}
