4c01ef2e36c4226f01ff69b37210daf867f4f5ed
Malte Bargholz <malte@screenri.de>
2017-07-26 02:45:27 +0200

    bring stub together

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ let express = require('express'); //import express framework
 let app = express();  //get the global express instance
 let port = process.env.PORT || 3000; //use supplied port or default 3000;
 
+//import sop routes
+let sop_router = require('./api/routes/sop_routes');
+
+//use the SOP router for the /sop/ prefix
+app.use('/sop/', sop_router);
+
 app.listen(port, () => console.log("express started")); //actually start the server and print a simple 
                                                         //message to notify the user something is happening
 
