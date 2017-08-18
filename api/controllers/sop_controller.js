34db7be1a5769e5b4caf17fecf5b23373ccf2fd1
Malte Bargholz <malte@screenri.de>
2017-08-18 23:56:14 +0200

    update sop controlelr for new model

--- api/controllers/sop_controller.js
+++ api/controllers/sop_controller.js
@@ -1,13 +1,17 @@
 'use strict;' //strict compiler mode. see main.js
 
 //import ORM Object
-const User = require('../model/sop_model')
+const Model = require('../model/sop_model')
+const Protocol = Model.Protocol;
 
 //a route function which returns all SOPs which are currently in the database
-function get_all_sops() {
-     return User.findAll();
+function get_all_protocols(req, res) {
+    Protocol.findAll()
+        .then((proto) => res.json(proto))
+        .catch((error) => res.send(error));
 }
 
+
 module.exports = {
-    get_all: get_all_sops //export the function get_all_sops under the name get_all
+    get_all: get_all_protocols //export the function get_all_sops under the name get_all
 }
