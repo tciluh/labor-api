b96f73b08665af14a6e82d167c7bf88a5819a7f3
Malte Bargholz <malte@screenri.de>
2017-09-05 19:31:12 +0200

    use camel case syntax for sop controller

--- api/controllers/sop_controller.js
+++ api/controllers/sop_controller.js
@@ -5,7 +5,7 @@ const Model = require('../model/sop_model')
 const Protocol = Model.Protocol;
 
 //a route function which returns all SOPs which are currently in the database
-function get_all_protocols(req, res) {
+function getAllProtocols(req, res) {
     Protocol.findAll()
         .then((proto) => res.json(proto))
         .catch((error) => res.send(error));
@@ -13,5 +13,5 @@ function get_all_protocols(req, res) {
 
 
 module.exports = {
-    get_all: get_all_protocols //export the function get_all_sops under the name get_all
+    get_all: getAllProtocols//export the function get_all_sops under the name get_all
 }
