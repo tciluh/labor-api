559d9d2063a6ea88d7ff67d396c3ce8cc33dea93
Malte Bargholz <malte@screenri.de>
2017-09-05 19:35:54 +0200

    add some explanation to user routes

--- api/routes/user_routes.js
+++ api/routes/user_routes.js
@@ -6,18 +6,25 @@ let router = express.Router(); //create a new router for the Users
 let controller = require('../controllers/user_controller');//import user controller
 
 //define root route
+//on a GET return all Users
 router.route('/')
-    .get(controller.getAll); //on a get return all Userss
+    .get(controller.getAll); 
 
+//on POST add a user
 router.route('/')
     .post(controller.add);
 
+//id specific routes
+
+//on GET get a user with the given id
 router.route('/:id')
     .get(controller.get);
 
+//on POST update a user with the given id
 router.route('/:id')
     .post(controller.update);
 
+//on DELETE delete a user with the given id
 router.route('/:id')
     .delete(controller.delete);
 
