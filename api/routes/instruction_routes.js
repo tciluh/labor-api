7f7c5046b662e5d203e82b838273976eb589b234
Malte Bargholz <malte@screenri.de>
2017-09-11 20:27:39 +0200

    implement instruction and result routes

--- api/routes/instruction_routes.js
+++ api/routes/instruction_routes.js
@@ -4,3 +4,24 @@ let express = require('express'); //import express
 let router = express.Router(); //create a new router for the SOPs
 
 let controller = require('../controllers/instruction_controller');//import sop controller
+
+//we need the async utility
+let syncify = require('../middleware/async_util');
+
+//we only support add, update, get for instructions
+//delete is handled by deleting a linked protocol
+//and there shouldnt be a way to get all instructions.
+
+//on POST add an instruction with parameters in the http body
+router.route('/')
+    .post(syncify(controller.add));
+
+//on GET return an instruction with the given id
+router.route('/:id')
+    .get(syncify(controller.get));
+
+//on PUT update an instruction with the given id
+router.route('/:id')
+    .put(syncify(controller.update));
+
+module.exports = router;
--- api/routes/result_routes.js
+++ api/routes/result_routes.js
@@ -1,6 +1,26 @@
-'use strict;'//use strict compiler mode. see main.js
+'use strict;' //use strict compiler mode. see main.js
 
 let express = require('express'); //import express
 let router = express.Router(); //create a new router for the SOPs
 
-let controller = require('../controllers/result_controller');//import sop controller
+let controller = require('../controllers/result_controller'); //import sop controller
+//we need the async utility
+let syncify = require('../middleware/async_util');
+
+//we only support add, update, get for results
+//delete is handled by deleting a linked protocol
+//and there shouldnt be a way to get all results.
+
+//on POST add a result with parameters in the http body
+router.route('/')
+    .post(syncify(controller.add));
+
+//on GET return a result with the given id
+router.route('/:id')
+    .get(syncify(controller.get));
+
+//on PUT update a result with the given id
+router.route('/:id')
+    .put(syncify(controller.update));
+
+module.exports = router;
