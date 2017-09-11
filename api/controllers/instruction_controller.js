9f8fcf530173735be424a049b51ff830438d2c93
Malte Bargholz <malte@screenri.de>
2017-09-11 20:27:11 +0200

    implement result and instruction controllers

--- api/controllers/instruction_controller.js
+++ api/controllers/instruction_controller.js
@@ -4,4 +4,40 @@
 const Model = require('../model/sop_model')
 const Instruction = Model.Instruction;
 
+async function addInstruction(req, res, next) {
+    const instr = await Instruction.create(req.body);
+    //return the created instruction.
+    res.json(result);
+}
+async function getInstruction(req, res, next) {
+    //find by id
+    const instr = Instruction.findById(req.params.id);
+    //check if we found something.
+    if (instr) {
+        //return 
+        res.json(instr);
+    } else {
+        next("no instruction with the given id found.");
+    }
 
+}
+async function updateInstruction(req, res, next) {
+    //get the instruction to update
+    let instr = Instruction.findById(req.params.id);
+    //check if there is an instruction to update
+    if (!instr) {
+        next("no instruction with the given id found.")
+    }
+    //perform the actual updaet
+    await instr.update(req.body);
+    //return the updated instruction
+    res.json(instr);
+
+
+}
+
+module.exports = {
+    get: getInstruction,
+    add: addInstruction,
+    update: updateInstruction,
+}
--- api/controllers/result_controller.js
+++ api/controllers/result_controller.js
@@ -1,5 +1,40 @@
 'use strict;' //strict compiler mode. see main.js
 
 //import ORM Object
-const Model = require('../model/sop_model')
+const Model = require('../model/sop_model');
 const Result = Model.Result;
+
+async function addResult(req, res, next) {
+    const result = await Result.create(req.body);
+    //return the created result
+    res.json(result);
+}
+async function getResult(req, res, next) {
+    //find by id
+    const result = Result.findById(req.params.id);
+    //if there is result with the given id
+    if (result) {
+        //return it
+        res.json(result);
+    } else {
+        next("no result with the given id found.");
+    }
+
+}
+async function updateResult(req, res, next) {
+    //get the result to update
+    let result = Result.findById(req.params.id);
+    if (!result) {
+        next("no result with the given id found.");
+    }
+    //perform the actual update
+    await result.update(req.body);
+    //return the updated result
+    res.json(result);
+}
+
+module.exports= {
+    get: getResult,
+    add: addResult,
+    update: updateResult,
+}
