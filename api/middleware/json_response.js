tciluh/labor-api

commit ab2df0fb1aaac14785aeff5af8b8b00219173578

wrap json reponses in a success/failure object
- this add a new middle ware which registers jsonSuccess() and
jsonError() on the response object
- also es6ify all string concatenations

api/middleware/json_response.js
@@ -1,0 +1,24 @@
+"use strict;"
+
+module.exports = (req, res, next) => {
+    //a middleware which register 2 new
+    //functions jsonSucess and jsonError which wrap the response object in another object
+    //with the structure
+    //{
+    //  success: true/false,
+    //  payload: the response
+    //}
+    res.jsonSuccess = (payload)  => {
+        return res.json({
+            success: true,
+            payload: payload
+        });
+    }
+    res.jsonError = (payload) => {
+        return res.json({
+            success: false,
+            payload: payload
+        });
+    }
+
+}

api/controllers/image_controller.js
@@ -10,15 +10,17 @@ const appRoot = require('app-root-path');
 
 //routing function for adding an image
 async function addImage(req, res, next) {
+    if(!req.file) next("image upload failed, file is not defined after upload");
     const image = await Image.create({
         filename: req.file.filename,
         contentType: req.file.mimetype //needs to be saved to allow proper downloading
     });
-    res.json(image);
+    res.jsonSuccess(image);
 }
 
 //routing function for updating an image
 async function updateImage(req, res, next){
+    if(!req.file) next("image update failed, file is not defined after upload");
     //find the image in question
     let image = await Image.findById(req.params.id);
     if(!image){
@@ -33,9 +35,9 @@ async function updateImage(req, res, next){
         contentType: req.file.mimetype //needs to be saved to allow proper downloading
     });
     //delete the old file
-    await del(['images/' + oldFilename]);
+    await del([`images/${oldFilename}`]);
     //return the updated image db entry
-    res.json(image);
+    res.jsonSuccess(image);
 }
 
 //routing function for deleting an image
@@ -51,7 +53,9 @@ async function deleteImage(req, res, next){
     //delete from db
     await image.destroy();
     //delete image file from disk
-    await del(['images/' + filename]);
+    await del([`images/${filename}`]);
+    //return a successful response
+    res.jsonSuccess(`Image with id: ${req.params.id} succesfully deleted`);
 }
 
 //routing function for getting an image
@@ -67,7 +71,7 @@ async function getImage(req, res, next){
     //based on the extension
     res.type(image.contentType);
     //send the file
-    res.sendFile('images/' + image.filename, { root: appRoot.toString()});
+    res.sendFile(`images/${image.filename}`, { root: appRoot.toString()});
 }
 
 

api/controllers/instruction_controller.js
@@ -10,7 +10,7 @@ async function getInstruction(req, res, next) {
     //check if we found something.
     if (instr) {
         //return 
-        res.json(instr);
+        res.jsonSuccess(instr);
     } else {
         next("no instruction with the given id found.");
     }
@@ -30,7 +30,7 @@ async function updateInstruction(req, res, next) {
     //perform the actual update
     await instr.update(req.body, allowedFields);
     //return the updated instruction
-    res.json(instr);
+    res.jsonSuccess(instr);
 
 
 }

api/controllers/protocol_controller.js
@@ -38,13 +38,13 @@ const findOptions = {
 //a route function which returns all Protocols(/SOPs) which are currently in the database
 async function getAllProtocols(req, res, next) {
     const protocols = await Protocol.findAll(findOptions);
-    res.json(protocols);
+    res.jsonSuccess(protocols);
 }
 
 async function getProtocol(req, res, next) {
     const protocol = await Protocol.findById(req.params.id, findOptions);
     if (protocol) {
-        res.json(protocol);
+        res.jsonSuccess(protocol);
     } else {
         next('no protocol found with the given id');
     }
@@ -138,7 +138,7 @@ async function addProtocol(req, res, next) {
         await instance.setTargetInstruction(targetInstruction);
     }
     //return the created protocol
-    res.json(await Protocol.findById(protocol.id, findOptions));
+    res.jsonSuccess(await Protocol.findById(protocol.id, findOptions));
 }
 
 async function updateProtocol(req, res, next) {
@@ -151,7 +151,7 @@ async function updateProtocol(req, res, next) {
     //perform the update
     await protocol.update(req.body, updateableFields);
     //return the updated protocol
-    res.json(protocol);
+    res.jsonSuccess(protocol);
 }
 
 async function deleteProtocol(req, res, next) {
@@ -177,8 +177,8 @@ async function deleteProtocol(req, res, next) {
     //finally delete the protocol itself
     await protocol.destroy();
 
-    //send some kind of response back
-    res.send("deletion sucessful");
+    //send success response back
+    res.jsonSuccess(`protocol with id: ${req.params.id} successfully deleted`);
 }
 
 module.exports = {

api/controllers/result_controller.js
@@ -10,7 +10,7 @@ async function getResult(req, res, next) {
     //if there is result with the given id
     if (result) {
         //return it
-        res.json(result);
+        res.jsonSuccess(result);
     } else {
         next("no result with the given id found.");
     }
@@ -29,7 +29,7 @@ async function updateResult(req, res, next) {
     //perform the actual update
     await result.update(req.body, allowedFields);
     //return the updated result
-    res.json(result);
+    res.jsonSuccess(result);
 }
 
 module.exports= {

api/controllers/user_controller.js
@@ -8,13 +8,13 @@ const User = Model.User;
 //a route function which returns all Users which are currently in the database
 async function getAllUsers(req, res, next) {
     const users = await User.findAll();
-    res.json(users);
+    res.jsonSuccess(users);
 }
 
 //a route function to add a User
 async function addUser(req, res, next) {
     const user = await User.create(req.body);
-    res.json(user);
+    res.jsonSuccess(user);
 }
 
 //a route function to delete a user
@@ -26,7 +26,7 @@ async function deleteUser(req, res, next){
         next("no user found with the given id");
     }
     await user.destroy();
-    res.send("User with ID: " + req.params.id + " sucessfully deleted");
+    res.jsonSuccess(`User with ID:  ${req.params.id} sucessfully deleted`);
 }
 
 //a route to update the info of a user
@@ -41,14 +41,14 @@ async function updateUser(req, res, next){
     //perform the update
     await user.update(req.body);
     //return the updated user
-    res.json(user);
+    res.jsonSuccess(user);
 }
 
 //a route function to get a certain user
 async function getUser(req, res, next) {
     const user = await User.findById(req.params.id);
     if(user){
-        res.json(user);
+        res.jsonSuccess(user);
     }
     else{
         next("no user found with the given id");

api/middleware/async_util.js
@@ -6,10 +6,18 @@
 //passing any error onto express
 function syncify(fn){
     return (req, res, next) => {
-        return Promise
-            .resolve(fn(req, res, next)) // this will resolve if none of
-                                         // the encapsulated await statements reject.
-            .catch(error => next(error));//otherwise this will be called and we call the express error handler
+        try{
+            return Promise
+                .resolve(fn(req, res, next)) // this will resolve if none of
+                                             // the encapsulated await statements reject.
+                .catch(error => next(error));//otherwise this will be called and we call the express error handler
+        }
+        catch(error){
+            //another catch block to catch any error thrown via
+            //throw error
+            //these are not catch'ed by the promise statement
+            next(error);
+        }
     }
 }
 

api/middleware/error_handling.js
@@ -8,7 +8,7 @@
 function sendRequestError(err, req, res, next){
     //set the status code to 500
     //and return a json error object
-    res.status(500).json({
+    res.status(500).jsonError({
         url: req.url,
         body: req.body,
         method: req.method,
@@ -19,6 +19,7 @@ function sendRequestError(err, req, res, next){
     next(err);
 }
 
+//a function which logs the error to the console
 function logRequestError(err, req, res, next){
     console.warn('an error ocurred while performing a request');
     console.error(JSON.stringify({

commit 8da7b248f66eae89aae1ecb81827245e7d75ecf2

fix api not answering any request
this fixes a regression introduced in
ab2df0fb1aaac14785aeff5af8b8b00219173578 which caused any request to
get stuck in the jsonSuccess/Error middleware since it forgot to call
next().

api/middleware/json_response.js
@@ -20,5 +20,6 @@ module.exports = (req, res, next) => {
             payload: payload
         });
     }
+    next();
 
 }