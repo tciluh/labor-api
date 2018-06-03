792994d240f3f059dc21ddf53fb3888d7bc42c79
Malte Bargholz <malte@screenri.de>
2018-06-03 16:52:04 +0200

    lint photometer.js

--- api/io/plugins/photometer.js
+++ api/io/plugins/photometer.js
@@ -1,40 +1,38 @@
-const PluginBase = require('../base');
-const SerialPort = require('serialport');
-const util = require('util');
+const PluginBase = require('../base')
+const SerialPort = require('serialport')
 
-let port;
+let port
 
-class Photometer extends PluginBase{
-    constructor(){
-        const identifier = 'photometer';
+class Photometer extends PluginBase {
+    constructor () {
+        const identifier = 'photometer'
         const actions = [
             'measure',
             'setTurret',
             'zero',
             'setWavelength'
-        ];
+        ]
         const serialportPath = '/dev/ttyUSB0'
         const serial = new SerialPort(serialportPath, {
             autoOpen: true,
             baudRate: 19200,
             dataBits: 8,
             stopBits: 1,
-            parity: 'none',
+            parity: 'none'
         })
-        //call super with the required parameters
-        super(identifier, actions);
+        // call super with the required parameters
+        super(identifier, actions)
         this.handlerMap = actions.reduce((acc, elem) => {
-            acc[elem] = this[elem];
-            return acc;
-        }, {});
-        port = serial;
-
+            acc[elem] = this[elem]
+            return acc
+        }, {})
+        port = serial
     }
-    handler(action, args){
+    handler (action, args) {
         return new Promise((resolve, reject) => {
-            port.write('\r\n\r\n');
-            //beep once to signal ready
-            port.write('\r\nBEEP\r\n');
+            port.write('\r\n\r\n')
+            // beep once to signal ready
+            port.write('\r\nBEEP\r\n')
             port.drain()
             this.handlerMap[action](args)
                 .then(result => resolve(result))
@@ -42,78 +40,77 @@ class Photometer extends PluginBase{
         })
     }
 
-    setTurret(args) {
-        let pos = args.position;
-        if(pos instanceof String  && (pos.toUpperCase() == 'B' || pos.toUpperCase() == 'BLANK')) {
-            //BLANK position == Position 0
-            pos = 0; 
+    setTurret (args) {
+        let pos = args.position
+        if (pos instanceof String && (pos.toUpperCase() === 'B' || pos.toUpperCase() === 'BLANK')) {
+            // BLANK position == Position 0
+            pos = 0
         }
-        if(!(pos instanceof Number)){
-            pos = Number.parseInt(pos);
+        if (!(pos instanceof Number)) {
+            pos = Number.parseInt(pos)
         }
-        if(Number.isNaN(pos) || !Number.isFinite(pos) || !Number.isInteger(pos)) {
-            throw new Error(`cant set turret to invalid position ${pos} (raw: ${args.position})`);
+        if (Number.isNaN(pos) || !Number.isFinite(pos) || !Number.isInteger(pos)) {
+            throw new Error(`cant set turret to invalid position ${pos} (raw: ${args.position})`)
         }
-        if(pos < 0 || pos > 5) {
-           throw new Error(`turret position ${pos} is out-of-bounds`);
-        } 
+        if (pos < 0 || pos > 5) {
+            throw new Error(`turret position ${pos} is out-of-bounds`)
+        }
         return new Promise((resolve, reject) => {
             port.write(`\r\nTURRET ${pos}\r\n`, 'ascii', (error) => {
-                if(error) reject(error)
+                if (error) reject(error)
                 resolve()
-            });
+            })
         })
     }
 
-    setWavelength(args) {
-        let wl = args.wavelength;
-        if(!(wl instanceof Number)){
-            wl = Number.parseInt(wl);
+    setWavelength (args) {
+        let wl = args.wavelength
+        if (!(wl instanceof Number)) {
+            wl = Number.parseInt(wl)
         }
-        if(Number.isNaN(wl) || !Number.isFinite(wl) || !Number.isInteger(wl)) {
-            throw new Error(`cant set wavelength to invalid value: ${wl} (raw: ${args.wavelength})`);
+        if (Number.isNaN(wl) || !Number.isFinite(wl) || !Number.isInteger(wl)) {
+            throw new Error(`cant set wavelength to invalid value: ${wl} (raw: ${args.wavelength})`)
         }
-        if(wl < 190 || wl > 1100) {
-            throw new Error(`wavelength ${wl} is out-of-bounds`);
+        if (wl < 190 || wl > 1100) {
+            throw new Error(`wavelength ${wl} is out-of-bounds`)
         }
         return new Promise((resolve, reject) => {
             port.write(`\r\nGOTO ${wl}\r\n`, 'ascii', (error) => {
-                if(error) reject(error)
+                if (error) reject(error)
                 resolve()
-            });
+            })
         })
     }
 
-    zero(args) {
+    zero (args) {
         return new Promise((resolve, reject) => {
             port.write(`\r\nZERO\r\n`, 'ascii', (error) => {
-                if(error) reject(error)
+                if (error) reject(error)
                 resolve()
-            });
+            })
         })
     }
 
-    measure(args) {
+    measure (args) {
         return new Promise((resolve, reject) => {
             port.flush((error) => {
-                if(error) reject(error)
+                if (error) reject(error)
                 port.on('data', (data) => {
-                    if(!data) return;
-                    const string = data.toString()
-                    log.error(`recieved: ${data}`);
-                    if(data.toString('ascii').includes('A') && !data.toString('ascii').includes('SAMPLE')){
+                    if (!data) return
+                    log.error(`recieved: ${data}`)
+                    if (data.toString('ascii').includes('A') && !data.toString('ascii').includes('SAMPLE')) {
                         const string = data.toString('ascii')
                             .replace('A', '')
                             .trim()
-                        resolve(string.substr(0, string.indexOf("\n")))
+                        resolve(string.substr(0, string.indexOf('\n')))
                     }
                 })
                 port.write('\r\nSAMPLE\r\n', 'ascii', (error) => {
-                    if(error) reject(error)
-                })	
+                    if (error) reject(error)
+                })
             })
         })
     }
 }
 
-module.exports = Photometer;
+module.exports = Photometer
